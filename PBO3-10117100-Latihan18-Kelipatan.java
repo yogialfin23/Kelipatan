dcce883b762077e6285c18edca8c7ca44d011755
yogialfin23 <43507570+yogialfin23@users.noreply.github.com>
2018-10-01 23:17:06 +0700

    Add files via upload

---
+++ PBO3-10117100-Latihan18-Kelipatan.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Yogir Alfin Salim
+ * 10117100
+ * IF-3
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double i = 3.5;
+        while (i <= 35) {
+            System.out.println(i);
+
+            i = i + 3.5;
+        }
+    }
+}
